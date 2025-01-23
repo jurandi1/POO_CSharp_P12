de33d7a9794de445501a0d7757f3c9470eccdabb
Jurandi Junior <jurandijr20@gmail.com>
2025-01-22 23:30:10 -0300

    Herança

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace POO_CSharp_P11
+﻿using POO_CSharp_P12.Entities;
+
+namespace POO_CSharp_P11
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            BusinessAccount account = new BusinessAccount(810, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0;
         }
     }
 }
